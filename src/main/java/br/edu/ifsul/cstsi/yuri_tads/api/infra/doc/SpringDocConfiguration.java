7d2bcc28a3e24926a72b6865f79303048561c903
Yuri <yuricaetano100@gmail.com>
2024-09-04 10:18:42 -0300

    feature: controllers, services, autentication JWT

---
+++ src/main/java/br/edu/ifsul/cstsi/yuri_tads/api/infra/doc/SpringDocConfiguration.java
@@ -0,0 +1,37 @@
+package br.edu.ifsul.cstsi.yuri_tads.api.infra.doc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //indica que essa classe deve ser adicionada ao Contexto do aplicativo como um Bean de Configuração da Documentação da API
+public class SpringDocConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API de Exemplo Desenvolvida para as Aulas de TADS")
+                        .description("API Rest da aplicação tads_produtos_springboot_exemplo, contendo as funcionalidades de CRUD de produtos.")
+                        .contact(new Contact()
+                                .name("Time Aulas TADS")
+                                .email("tads@ifsul.edu.br"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://localhost:8080/api/licenca")));
+    }
+}
